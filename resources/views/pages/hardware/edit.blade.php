f316ad3ddfe32a75a03fce9e1bfc9bd1a4ae1f61
Samuel Sukanda <samuelsukanda24@gmail.com>
2026-08-07 14:33:12 +0700

    feat: add detail and edit views for hardware checklist management

--- resources/views/pages/hardware/edit.blade.php
+++ resources/views/pages/hardware/edit.blade.php
@@ -59,6 +59,8 @@
                                                     'Cleaning CPU & Cek Pengkabelan',
                                                     'Hapus cache temp dan cache browser',
                                                     'Akses Flashdisk terkontrol',
+                                                    'Akses Flashdisk terkontrol',
+                                                    'Running SFC & DISM',
                                                 ];
 
                                                 $checklistData = $hardware->checklist ?? [];
